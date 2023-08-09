271e7a7597cfe830aafacdae5ee96e90a0107c2b
Bogdan Smaluniuk <bogdansmalinuk@gmail.com>
2023-08-09 16:25:55 +0300

    &quot;

--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -7,8 +7,8 @@ export default function About() {
                 <div className={styles.content}>
                     <h2>About game</h2>
                     <p>
-                        A card memory game, often known as a "memory matching game" or "concentration game," is a popular type of
-                        single-player or multiplayer game that challenges players' memory and cognitive skills.</p>
+                        A card memory game, often known as a &quot;memory matching game&quot; or &quot;concentration game,&quot; is a popular type of
+                        single-player or multiplayer game that challenges players memory and cognitive skills.</p>
                     <p>The objective of the game is to find pairs of matching cards from a shuffled set of facedown cards laid out in a grid formation.
                         It is commonly played using a deck of standard playing cards or custom cards with images, numbers, or symbols.</p>
                 </div>
