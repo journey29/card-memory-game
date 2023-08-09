3076cadd1c6e8c0745b3d2988f1237415f0fa945
Bogdan Smaluniuk <bogdansmalinuk@gmail.com>
2023-08-09 16:30:02 +0300

    eslint rule

--- src/app/components/Gameplay/index.tsx
+++ src/app/components/Gameplay/index.tsx
@@ -9,7 +9,7 @@ export default function Gameplay() {
                     <ul>
                         <li>On each turn, a player selects two facedown cards.</li>
                         <li>If the two cards match, they are removed from the game board and the player earns points.</li>
-                        <li>If the cards do not match, they are flipped face down again, and the player&quot;s turn ends.</li>
+                        <li>If the cards do not match, they are flipped face down again, and the players turn ends.</li>
                         <li>Players take turns in succession until all pairs have been matched and removed from the board.</li>
                     </ul>
                 </div>
